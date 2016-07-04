5925bae9ea3a531a7cf45ed81243f7c07c5e9d7b
Meditate <aroundofyou@gmail.com>
2016-07-04 22:00:39 +0200

    Passing specs for :read permission

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,17 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    alias_action :index, :to => :readindex
+    alias_action :show, :to => :readshow
     user ||= User.new
 
     if user.is? :admin
       can :manage, User
-    else
-      can :read, User
+    end
+
+    if user.is? :user
+      can :readindex,User
+      can :readshow, User, :id => user.id
     end
 
     # Define abilities for the passed in user here. For example:
--- spec/abilities/user_ability_spec.rb
+++ spec/abilities/user_ability_spec.rb
@@ -1,20 +1,28 @@
 require "rails_helper"
 
 describe "abilities" do
+  let(:user) {create(:user)}
   let(:editor) {create(:user, :editor)}
-  let(:user){create(:user, :admin)}
+  let(:admin){create(:user, :admin)}
   let(:other_user) {FactoryGirl.build_stubbed :user}
-  subject (:ability){Ability.new(user)}
 
-  context 'when user is an admin' do
-    it{ should be_able_to(:manage, other_user) }
+  context 'when user is not loged in' do
+    subject(:ability){Ability.new(nil)}
+    it{should_not be_able_to(:manage, :all)}
   end
 
-  context 'when is an user' do
-    it{ should be_able_to(:read, other_user) }
+  context 'when user is an user' do
+    subject (:ability){Ability.new(user)}
+    it{ should be_able_to(:readshow, user) }
+    it{ should be_able_to(:readindex, User) }
+    it{ should_not be_able_to(:readshow, other_user)}
     it{ should_not be_able_to(:destroy,:update,:create, other_user)}
   end
 
+  context 'when user is an admin' do
+    subject (:ability){Ability.new(admin)}
+    it{ should be_able_to(:manage, other_user) }
+  end
 
 
 end
